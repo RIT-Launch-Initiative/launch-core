5eb14184062869bbf5ee1fa6aebd66bd9fc4d0f8
Aaron Chan <aarchan108@gmail.com>
2022-08-14 13:41:32 -0400

    String constructor and tests. Memory issues. Massive L

---
+++ string/lstring.h
@@ -0,0 +1,38 @@
+/**
+ * Library for embedded strings
+ * 
+ * @author Aaron Chan
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+#include <stdlib.h>
+#include <iostream>
+
+const char NULL_TERMINATOR = '\0';
+
+// template <const size_t size>
+class String {
+    public:
+        char* string;
+        int len;
+        size_t size;
+
+        String(char* buffer, size_t buff_size) {
+            this->string[buff_size];
+            int i;
+
+            for (i = 0; i < buff_size; i++) {
+                this->string[i] = buffer[i];
+            }
+
+            if (this->string[i] != NULL_TERMINATOR) {
+                this->string[i] = '\0';
+            }
+        }
+
+};
+
+
+#endif
---
+++ string/test/string_test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "string/lstring.h"
+
+
+int main() {
+    char* buffer = "RIT Launch Initiative";
+
+    std::cout << "Full String Test: " << std::endl;
+    String full_string = String(buffer, 22);
+    std::cout << "\tExpected: RIT Launch Initiative" << std::endl;
+    std::cout << "\tActual: " << full_string.string << std::endl;
+
+    std::cout << "Extra Space String Test: " << std::endl;
+    String much_storage_string = String(buffer, 999);
+    std::cout << "\tExpected: RIT Launch Initiative" << std::endl;
+    std::cout << "\tActual: " << much_storage_string.string << std::endl;
+
+    std::cout << "Truncated String Test: " << std::endl;
+    String trunc_string = String(buffer, 4);
+    std::cout << "\tExpected: RIT" << std::endl;
+    std::cout << "\tActual: " << trunc_string.string << std::endl;
+
+    return 0;
+}
