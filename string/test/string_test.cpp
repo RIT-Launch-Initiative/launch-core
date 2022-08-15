944663b913d72e2c95ba41cebfb5c09574ff614e
Aaron Chan <aarchan108@gmail.com>
2022-08-15 13:00:18 -0400

    Make buffer better

--- string/test/string_test.cpp
+++ string/test/string_test.cpp
@@ -3,20 +3,20 @@
 
 
 int main() {
-    char* buffer = "RIT Launch Initiative";
+    char buffer[] = {'R','I','T',' ','L','a','u','n','c','h',' ','I','n','i','t','i','a','t','i','v','e'};
 
     std::cout << "Full String Test: " << std::endl;
-    String full_string = String(buffer, 22);
+    auto full_string = String(buffer, 21);
     std::cout << "\tExpected: RIT Launch Initiative" << std::endl;
     std::cout << "\tActual: " << full_string.string << std::endl;
 
     std::cout << "Extra Space String Test: " << std::endl;
-    String much_storage_string = String(buffer, 999);
+    auto much_storage_string = String(buffer, 999);
     std::cout << "\tExpected: RIT Launch Initiative" << std::endl;
     std::cout << "\tActual: " << much_storage_string.string << std::endl;
 
     std::cout << "Truncated String Test: " << std::endl;
-    String trunc_string = String(buffer, 4);
+    auto trunc_string = String(buffer, 4);
     std::cout << "\tExpected: RIT" << std::endl;
     std::cout << "\tActual: " << trunc_string.string << std::endl;
 
