4d91454b93f34d432362a87f24cd02c6ff864731
Aaron Chan <aarchan108@gmail.com>
2022-08-15 19:42:10 -0400

    Formatting

--- string/string.h
+++ string/string.h
@@ -11,29 +11,28 @@
 
 const char NULL_TERMINATOR = '\0';
 
-template <const size_t size>
+template<const size_t size>
 class String {
-    public:
-        int len = 0;
-        char string[size] = {};
+public:
+    int len = 0;
+    char string[size] = {};
 
-    explicit String(char const* buffer) {
-            int i; // Reduce errors from using len as counter
+    explicit String(char const *buffer) {
+        int i; // Reduce errors from using len as counter
 
-            for (i = 0; i < size - 1; i++) {
-                this->string[i] = buffer[i];
+        for (i = 0; i < size - 1; i++) {
+            this->string[i] = buffer[i];
 
-                if (this->string[i] == NULL_TERMINATOR) break;
-            }
-
-            // Developers should still add a null terminator in their buffers, especially if len < buff_size
-            if (this->string[i] != NULL_TERMINATOR) {
-                this->string[i] = '\0';
-            }
-
-            this->len = i;
+            if (this->string[i] == NULL_TERMINATOR) break;
         }
 
+        // Developers should still add a null terminator in their buffers, especially if len < buff_size
+        if (this->string[i] != NULL_TERMINATOR) {
+            this->string[i] = '\0';
+        }
+
+        this->len = i;
+    }
 };
 
 
