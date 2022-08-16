c8d18bc3cc8c144f23d916777bfd0f5e7f257df6
Aaron Chan <aarchan108@gmail.com>
2022-08-15 20:46:25 -0400

    tester function

--- string/test/string_test.cpp
+++ string/test/string_test.cpp
@@ -1,24 +1,36 @@
 #include <cstdio>
 #include "string/string.h"
 
+
+void print_new_test(char* test_name, char* expected) {
+    printf("%s Test: \n", test_name);
+    printf("\tExpected: %s\n", expected);
+}
+
 int main() {
     char buffer[] = {'R','I','T',' ','L','a','u','n','c','h',' ','I','n','i','t','i','a','t','i','v','e', '\0'};
 
-    printf("Full String Test: \n");
+    // Init tests
     auto full_string = String<22>(buffer);
-    printf("\tExpected: RIT Launch Initiative\n");
+    print_new_test("Full String", "RIT Launch Initiative");
     printf("\tActual: %s\n", full_string.string);
 
-    printf("Extra Space String Test: \n");
     auto much_storage_string = String<999>(buffer);
-    printf("\tExpected: RIT Launch Initiative\n");
+    print_new_test("Extra Space String", "RIT Launch Initiative");
     printf("\tActual: %s\n", much_storage_string.string);
 
 
-    printf("Truncated String Test: \n");
     auto trunc_string = String<4>(buffer);
-    printf("\tExpected: RIT\n");
+    print_new_test("Truncated String", "RIT Launch Initiative");
     printf("\tActual: %s\n", trunc_string.string);
+
+    // Accessor Tests
+
+
+
+    // Modifier Tests
+
+
 
 
 
