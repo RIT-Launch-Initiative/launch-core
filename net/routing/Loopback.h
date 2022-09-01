c2a668cc2e69bb339d2c7ee2d3a81b5b30c0fbd7
WillMerges <wdmerges@gmail.com>
2022-09-01 17:17:00 -0400

    added loopback device

---
+++ net/routing/Loopback.h
@@ -0,0 +1,25 @@
+#ifndef LOOPBACK_H
+#define LOOPBACK_H
+
+#include "return.h"
+#include "net/routing/NetworkLayer.h"
+
+/// @brief simple network layer that loops packets back to the caller
+class Loopback : public NetworkLayer {
+public:
+    /// @brief constructor
+    Loopback() {};
+
+    /// @brief transmit
+    RetType transmit(Packet& packet, msg_t& info, NetworkLayer* caller) {
+        return caller->receive(packet, info, this);
+    }
+
+    /// @brief receive
+    /// @return always error, loopback cannot receive
+    RetType receive(Packet&, msg_&, NetworkLayer*) {
+        return RET_ERROR;
+    }
+};
+
+#endif
