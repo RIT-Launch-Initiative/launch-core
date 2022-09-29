7bb3ce5bec10de0661a9729a17d759f06c0fdf37
Aaron Chan <aarchan108@gmail.com>
2022-09-29 18:14:50 -0400

    send packet

---
+++ net/transport_layer/udp.h
@@ -0,0 +1,24 @@
+#ifndef LAUNCH_CORE_UDP_H
+#define LAUNCH_CORE_UDP_H
+
+#include "../socket/Socket.h"
+#include "../packet/Packet.h"
+
+class UDP {
+public:
+    Socket socket; // TODO: Is raw necessary?
+
+    UDP(sockaddr_t* addr) {
+        socket = Socket().init;
+    }
+
+    RetType send_packet(uint8_t* data) {
+        int packet_size = 31 + sizeof(data) + 1;
+        const int header_size = 8;
+        Packet packet = alloc::Packet<const_cast<int>(header_size), header_size>();
+        packet.push(data);
+    }
+};
+
+
+#endif //LAUNCH_CORE_UDP_H
