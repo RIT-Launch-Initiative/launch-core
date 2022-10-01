709be949c186c78762d72e5642f53912a64c9557
Aaron Chan <aarchan108@gmail.com>
2022-10-01 11:21:03 -0400

    Calculate checksum

--- net/transport_layer/udp.h
+++ net/transport_layer/udp.h
@@ -1,8 +1,8 @@
 #ifndef LAUNCH_CORE_UDP_H
 #define LAUNCH_CORE_UDP_H
 
-#include "socket/Socket.h"
-#include "packet/Packet.h"
+#include "../socket/Socket.h"
+#include "../packet/Packet.h"
 #include ""
 
 class UDP : public NetworkLayer {
@@ -11,12 +11,28 @@ public:
 
     }
 
+    int calc_checksum(uint8_t* payload) {
+        // CRC8 checksum
+
+        int checksum = 0;
+        for (int i = 0; i < sizeof(payload); i++) {
+            checksum += payload[i];
+        }
+
+        return checksum;
+    }
+
     RetType receive(Packet &packet, sockmsg_t &info, NetworkLayer *caller) {
+        int checksum = calc_checksum(info.payload);
+
 
         return RET_SUCCESS;
     }
 
     RetType transmit(Packet &packet, sockmsg_t &info, NetworkLayer *caller) {
+        int checksum = calc_checksum(info.payload);
+
+
 
         return RET_SUCCESS;
     }
