cfe105a652ea288f4526d46b220e99fa1913c715
Aaron Chan <aarchan108@gmail.com>
2022-11-05 12:33:56 -0400

    Checksum calculation

--- net/udp/udp.h
+++ net/udp/udp.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include "net/socket/Socket.h"
+#include "net/ipv4/ipv4.h"
 
 namespace udp {
     typedef struct {
@@ -12,31 +13,19 @@ namespace udp {
         uint16_t src;
     } UDP_HEADER_T;
 
-    // TODO: Need to pass in headers
-    uint16_t checksum(UDP_HEADER_T udp_hdr, const uint16_t* data, uint16_t len) {
+    uint16_t checksum(ipv4::IPv4Addr_t src, ipv4::IPv4Addr_t dst, const uint16_t *data, uint16_t len) {
         uint16_t sum = 0;
 
-        // TODO: Calculate IP psuedo header
+        sum += src;
+        sum += dst;
+        sum += static_cast<uint8_t>(0x11);
+        sum += len;
 
-
-        sum += udp_hdr.src;
-        sum += udp_hdr.dst;
-        sum += udp_hdr.checksum;
-        sum += udp_hdr.length;
-
-
-
-        for(int i = 0; i < (len / sizeof(uint16_t)); i++) {
+        for (int i = 0; i < len; i++) {
             sum += data[i];
         }
 
         return ~sum;
-    }
-
-
-    bool verify_checksum(UDP_HEADER_T header, uint16_t *payload, uint16_t actual_checksum) {
-        // TODO: Update size
-        return checksum(header, payload, sizeof(UDP_HEADER_T) + sizeof(*payload)) == actual_checksum;
     }
 }
 
