308b9ee03d4728fccbdaa06220cc9cfec7929946
Aaron Chan <aarchan108@gmail.com>
2022-11-16 19:30:26 -0500

    Stub out File and filesystem classes

---
+++ filesystem/File.h
@@ -0,0 +1,16 @@
+/**
+ * Name: File.h
+ * Purpose: Stores blocks of data
+ * Author: Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_FILE_H
+#define LAUNCH_CORE_FILE_H
+
+namespace filesystem {
+    class File {
+
+    };
+}
+
+#endif //LAUNCH_CORE_FILE_H
---
+++ filesystem/Filesystem.h
@@ -0,0 +1,16 @@
+/**
+ * Name: Filesystem.h
+ * Purpose: Provide an interface managing how files are stored and retrieved
+ * Author: Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_FILESYSTEM_H
+#define LAUNCH_CORE_FILESYSTEM_H
+
+namespace filesystem {
+    class Filesystem() {
+
+    }
+}
+
+#endif //LAUNCH_CORE_FILESYSTEM_H
