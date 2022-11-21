b93b064fc6650af5b492d2cabbd70f7d8dde7836
Aaron Chan <aarchan108@gmail.com>
2022-11-21 09:14:01 -0500

    Initial header for w25q

---
+++ device/peripherals/w25q/w25q.h
@@ -0,0 +1,10 @@
+/**
+ * Platform Independent Implementation for a W25Q Flash Memory Chip
+ *
+ * @author Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_W25Q_H
+#define LAUNCH_CORE_W25Q_H
+
+#endif //LAUNCH_CORE_W25Q_H
