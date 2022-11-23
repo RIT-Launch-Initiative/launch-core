f7f50ca5416775bac53682d369b2a5d0286a50e2
Aaron Chan <aarchan108@gmail.com>
2022-11-23 13:59:51 -0500

    Read commands

--- device/peripherals/w25q/w25q.h
+++ device/peripherals/w25q/w25q.h
@@ -7,4 +7,51 @@
 #ifndef LAUNCH_CORE_W25Q_H
 #define LAUNCH_CORE_W25Q_H
 
+#include "device/I2CDevice.h"
+#include "device/SPIDevice.h"
+
+
+class W25Q {
+public:
+    typedef enum {
+        READ_DATA = 0x03,
+        FAST_READ = 0x00,
+
+        FAST_READ_DUAL = 0x03,
+        FAST_READ_QUAD = 0x06,
+
+        FAST_READ_DUAL_IO = 0x0B,
+        FAST_READ_QUAD_IO = 0x0E,
+
+        WORD_READ_QUAD_IO = 0xE7,
+        OCTAL_WORD_READ_QUAD_IO = 0xE3,
+    } READ_COMMAND_T;
+
+    W25Q(SPIDevice& spiDevice) : spiDevice(spiDevice) {}
+
+    RetType read() {
+        const uint8_t READ_COMMAND = 0x03;
+
+
+        return RET_SUCCESS;
+    }
+
+
+    RetType readData(uint32_t address) {
+        const uint8_t READ_COMMAND = 0x03;
+
+
+
+        return RET_SUCCESS;
+    }
+
+    RetType write() {
+        return RET_SUCCESS;
+    }
+
+private:
+    SPIDevice& spiDevice;
+
+};
+
 #endif //LAUNCH_CORE_W25Q_H
