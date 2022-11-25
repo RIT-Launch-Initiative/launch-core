6a128ab11270c122dd9f49863765c1e18cc582e6
Aaron Chan <aarchan108@gmail.com>
2022-11-25 18:41:26 -0500

    Toggle function

--- device/peripherals/LED/RGB.h
+++ device/peripherals/LED/RGB.h
@@ -4,30 +4,90 @@
  * @author Aaron Chan
  */
 
-#include "device/peripherals/LED/LED.h"
+#include "device/GPIODevice.h"
+#include "sched/macros.h"
 
 #ifndef LAUNCH_CORE_RGB_H
 #define LAUNCH_CORE_RGB_H
 
 namespace LED {
     class RGB {
+        typedef enum {
+            RED,
+            ORANGE,
+            YELLOW,
+            GREEN,
+            BLUE,
+            INDIGO,
+            VIOLET
+        } RGB_COLOR_T;
+
     public:
-        RGB(GPIODevice& redPin, GPIODevice& bluePin, GPIODevice& greenPin) : redPin(redPin), bluePin(bluePin), greenPin(greenPin) {}
-        RGB(GPIODevice& redPin, GPIODevice& bluePin, GPIODevice& greenPin, uint8_t redVal, uint8_t blueVal, uint8_t greenVal) :
-        redPin(redPin), bluePin(bluePin), greenPin(greenPin), redVal(redVal), blueVal(blueVal), greenVal(greenVal) {}
+        RGB(GPIODevice &redPin, GPIODevice &greenPin, GPIODevice &bluePin) : redPin(redPin), greenPin(greenPin),
+                                                                             bluePin(bluePin) {}
+
+        RGB(GPIODevice &redPin, GPIODevice &greenPin, GPIODevice &bluePin, uint8_t redVal,
+            uint8_t greenVal, uint8_t blueVal) :
+                redPin(redPin), greenPin(greenPin), bluePin(bluePin), redVal(redVal), greenVal(greenVal),
+                blueVal(blueVal) {}
+
+        RetType toggle() {
+            uint32_t result = 0;
+
+            redPin.get(&result);
+            if (result != 0) return turnOff();
+
+            bluePin.get(&result);
+            if (result != 0) return turnOff();
+
+            greenPin.get(&result);
+            if (result != 0) return turnOff();
+
+            return setColors();
+        }
+
+        RetType setColor(uint8_t red, uint8_t blue, uint8_t green) {
+
+        }
+
+        RetType setColor(RGB_COLOR_T color) {
 
 
-
+        }
 
 
     private:
-        GPIODevice& redPin;
-        GPIODevice& bluePin;
-        GPIODevice& greenPin;
+        GPIODevice &redPin;
+        GPIODevice &bluePin;
+        GPIODevice &greenPin;
 
+        // TODO: Worth storing this just for an LED?
         uint8_t redVal = 255;
         uint8_t blueVal = 255;
         uint8_t greenVal = 255;
+
+        RetType turnOff() {
+            RESUME();
+
+            CALL(redPin.set(0));
+            CALL(bluePin.set(0));
+            CALL(greenPin.set(0));
+
+            RESET();
+            return RET_SUCCESS;
+        }
+
+
+        RetType setColors() {
+            RESUME();
+
+            CALL(redPin.set(redVal));
+            CALL(bluePin.set(blueVal));
+            CALL(greenPin.set(greenVal));
+
+            RESET();
+            return RET_SUCCESS;
+        }
 
     };
 }
