7a22f1e2af1bd91da104dcade18decfe71940bbe
Aaron Chan <aarchan108@gmail.com>
2022-11-25 18:21:22 -0500

    Realized I was off

--- device/peripherals/LED/RGB.h
+++ device/peripherals/LED/RGB.h
@@ -10,24 +10,17 @@
 #define LAUNCH_CORE_RGB_H
 
 namespace LED {
-    class RGB : LED {
+    class RGB {
     public:
-        RGB(GPIODevice &ledDevice) : LED(ledDevice), red(255), blue(255), green(255) {}
+        RGB(GPIODevice& redPin, GPIODevice& bluePin, GPIODevice greenPin) : redPin(redPin), bluePin(bluePin), greenPin(greenPin) {}
 
-        RGB(GPIODevice &ledDevice, LED_STATE_T state) : LED(ledDevice, state), red(255), blue(255), green(255) {}
-
-        RGB(GPIODevice &ledDevice, uint8_t red = 0, uint8_t blue = 0, uint8_t green = 0) : LED(ledDevice), red(red),
-                                                                                           blue(blue), green(green) {}
-
-        RGB(GPIODevice &ledDevice, LED_STATE_T state, uint8_t red = 0, uint8_t blue = 0, uint8_t green = 0) : LED(ledDevice, state), red(red),
-                                                                                      blue(blue), green(green) {}
 
 
 
     private:
-        uint8_t red;
-        uint8_t blue;
-        uint8_t green;
+        GPIODevice& redPin;
+        GPIODevice& bluePin;
+        GPIODevice& greenPin;
     };
 }
 
