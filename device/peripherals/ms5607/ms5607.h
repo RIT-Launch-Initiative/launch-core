ec81a1c13cb508e39178276ba123c9dbd64eb9bf
Aaron Chan <aarchan108@gmail.com>
2022-11-26 17:37:26 -0500

    Initial ms5607 class

---
+++ device/peripherals/ms5607/ms5607.h
@@ -0,0 +1,35 @@
+/**
+ * Implementation of driver for the MS5607 Altimeter
+ *
+ * @author Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_MS5607_H
+#define LAUNCH_CORE_MS5607_H
+
+#include "device/GPIODevice.h"
+
+typedef enum {
+    I2C_PROTOCOL = 1,
+    SPI_PROTOCOL = 0
+} MS5607_SERIAL_PROTOCOL_T;
+
+
+class MS5607 {
+public:
+    MS5607(GPIODevice &psPin, GPIODevice &diPin, GPIODevice &doPin, GPIODevice &csPin, GPIODevice &sdaPin) :
+            protocolSelectPin(psPin), dataInPin(diPin), dataOutPin(doPin), chipSelectPin(csPin), serialDataPin(sdaPin)
+    {}
+
+private:
+    MS5607_SERIAL_PROTOCOL_T selectedProtocol;
+    GPIODevice &protocolSelectPin;
+
+    GPIODevice &dataInPin;
+    GPIODevice &dataOutPin;
+    GPIODevice &chipSelectPin;
+
+    GPIODevice &serialDataPin;
+};
+
+#endif //LAUNCH_CORE_MS5607_H
