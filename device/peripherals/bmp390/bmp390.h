5ef5f8c28d4c82ba3845777243f153104dd050fb
Aaron Chan <aarchan108@gmail.com>
2022-11-27 14:15:26 -0500

    BMP 390 definitions

---
+++ device/peripherals/bmp390/bmp390.h
@@ -0,0 +1,16 @@
+/**
+ * Implementation of a Bosh Sensortec BMP390L
+ *
+ * @author Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_BMP390_H
+#define LAUNCH_CORE_BMP390_H
+
+class BMP390 {
+    BMP390() {
+
+    }
+};
+
+#endif //LAUNCH_CORE_BMP390_H
