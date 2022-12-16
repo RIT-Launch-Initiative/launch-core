ec801ec54b4d14c359339f7edf9df46c09d270b1
Aaron Chan <aarchan108@gmail.com>
2022-12-16 00:45:21 -0500

    callback fn

--- device/platforms/stm32/HAL_GPIO_HANDLERS.cpp
+++ device/platforms/stm32/HAL_GPIO_HANDLERS.cpp
@@ -8,14 +8,14 @@
 #include "device/platforms/stm32/HAL_Handlers.h"
 
 namespace HALHandlers {
-    static alloc::Hashmap<GPIO_HandleTypeDef*, dev_t, MAX_GPIO_DEVICES, MAX_GPIO_DEVICES> gpioMap;
+    static alloc::Hashmap<GPIO_HandleTypeDef *, dev_t, MAX_GPIO_DEVICES, MAX_GPIO_DEVICES> gpioMap;
 
-    RetType register_gpio(GPIO_HandleTypeDef* halGPIO, CallbackDevice* dev, int num) {
+    RetType register_gpio(GPIO_HandleTypeDef *halGPIO, CallbackDevice *dev, int num) {
         if (gpioMap[halGPIO] != NULL) {
             if (!gpioMap.rm(halGPIO)) return RET_ERROR;
         }
 
-        dev_t* pDev = gpioMap.add(halGPIO);
+        dev_t *pDev = gpioMap.add(halGPIO);
 
         if (pDev == NULL) return RET_ERROR;
 
@@ -25,5 +25,9 @@ namespace HALHandlers {
     }
 }
 
+void halGPIOCallback(GPIO_HandleTypeDef *halGPIO) {
+    HALHandlers::dev_t *dev = HALHandlers::gpioMap[halGPIO];
+    if (dev) dev->dev->callback(dev->num);
+}
 
 
