348680c4bc38c9ce305c2be8d63d39c84dcde5be
skippynose <devarapalli148@gmail.com>
2022-12-22 13:42:13 -0500

    Requested changes from the PR have been implemented.

--- device/peripherals/ADXL375/ADXL375.h
+++ device/peripherals/ADXL375/ADXL375.h
@@ -16,19 +16,15 @@
 
 class ADXL375 {
 public:
-    ADXL375(I2CDevice& i2c) : m_i2c(i2c)
-        {}
+    ADXL375(I2CDevice& i2c) : m_i2c(i2c) {}
 
    RetType readX(int16_t* xAxis){
-        // TODO read the values of the MSB and LSB and pass it into the final variable
-        RetType ret;
         RESUME();
-        uint8_t lsb;
-        uint8_t msb;
-        uint16_t value;
+        uint8_t* lsb;
+        uint8_t* msb;
 
         // reading the data
-        ret = CALL(m_i2c.read(xLSBData, lsb, 1));
+        RetType ret = CALL(m_i2c.read(xLSBData, lsb, 1));
         if (ret != RET_SUCCESS){
             RESET();
             return ret;
@@ -40,29 +36,20 @@ public:
             return ret;
         }
 
-        // combining the data
-        value |= msb << 8;
-        value |= lsb;
-
         // value is in 2's complement so have to convert it
-        int16_t adjustedValue = value;
-        adjustedValue *= -1;
-        *xAxis = adjustedValue;
+        *xAxis = ((msb << 8) | lsb) * -1;
 
         RESET();
-       return RET_SUCESS;
+        return RET_SUCCESS;
     }
 
     RetType readY(int16_t* yAxis){
-        // TODO read the values of the MSB and LSB and pass it into the final variable
-        RetType ret;
         RESUME();
-        uint8_t lsb;
-        uint8_t msb;
-        uint16_t value;
+        uint8_t* lsb;
+        uint8_t* msb;
 
         // reading the data
-        ret = CALL(m_i2c.read(yLSBData, lsb, 1));
+        RetType ret = CALL(m_i2c.read(yLSBData, lsb, 1));
         if (ret != RET_SUCCESS){
             RESET();
             return ret;
@@ -74,29 +61,21 @@ public:
             return ret;
         }
 
-        // combining the data
-        value |= msb << 8;
-        value |= lsb;
 
         // value is in 2's complement so have to convert it
-        int16_t adjustedValue = value;
-        adjustedValue *= -1;
-        *yAxis = adjustedValue;
+        *yAxis = ((msb << 8) | lsb) * -1;
 
         RESET();
         return RET_SUCCESS;
     }
 
     RetType readZ(int16_t* zAxis){
-        // TODO read the values of the MSB and LSB and pass it into the final variable
-        RetType ret;
         RESUME();
-        uint8_t lsb;
-        uint8_t msb;
-        uint16_t value;
+        uint8_t* lsb;
+        uint8_t* msb;
 
         // reading the data
-        ret = CALL(m_i2c.read(zLSBData, lsb, 1));
+        RetType ret = CALL(m_i2c.read(zLSBData, lsb, 1));
         if (ret != RET_SUCCESS){
             RESET();
             return ret;
@@ -108,14 +87,7 @@ public:
             return ret;
         }
 
-        // combining the data
-        value |= msb << 8;
-        value |= lsb;
-
-        // value is in 2's complement so have to convert it
-        int16_t adjustedValue = value;
-        adjustedValue *= -1;
-        *zAxis = adjustedValue;
+        *zAxis = ((msb << 8) | lsb) * -1;
 
         RESET();
         return RET_SUCCESS;
@@ -127,36 +99,12 @@ private:
     I2CDevice& m_i2c;
 
     // I2CAddr for the different axis data
-    I2CAddr_t xLSBData;
-    I2CAddr_t xMSBData;
-    I2CAddr_t yLSBData;
-    I2CAddr_t yMSBData;
-    I2CAddr_t zLSBData;
-    I2CAddr_t zMSBData;
-
-    xLSBData.dev_addr = 0x3B;
-    xLSBData.mem_addr = 0x32;
-    xLSBData.mem_addr_size = 1;
-
-    xMSBData.dev_addr = 0x3B;
-    xMSBData.mem_addr = 0x33;
-    xMSBData.mem_addr_size = 1;
-
-    yLSBData.dev_addr = 0x3B;
-    yLSBData.mem_addr = 0x34;
-    yLSBData.mem_addr_size = 1;
-
-    yMSBData.dev_addr = 0x3B;
-    yMSBData.mem_addr = 0x35;
-    yMSBData.mem_addr_size = 1;
-
-    zLSBData.dev_addr = 0x3B;
-    zLSBData.mem_addr = 0x36;
-    zLSBData.mem_addr_size = 1;
-
-    zMSBData.dev_addr = 0x3B;
-    zMSBData.mem_addr = 0x37;
-    zMSBData.mem_addr_size = 1;
+    I2CAddr_t xLSBData {0x3B, 0x32, 1};
+    I2CAddr_t xMSBData {0x3B, 0x33, 1};
+    I2CAddr_t yLSBData {0x3B, 0x34, 1};
+    I2CAddr_t yMSBData {0x3B, 0x35, 1};
+    I2CAddr_t zLSBData {0x3B, 0x36, 1};
+    I2CAddr_t zMSBData {0x3B, 0x37, 1};
 };
 
 
