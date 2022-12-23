31da639d6c488b5cfc989a266cff7f76a7ff7087
Aaron Chan <aarchan108@gmail.com>
2022-12-23 14:21:29 -0500

    Added driver files created by STM

---
+++ device/peripherals/LIS3MDL/LIS3MDL.h
@@ -0,0 +1,17 @@
+/**
+ * Platform Independent Implementation of the LIS3MDL sensor
+ *
+ * @author Aaron Chan
+ */
+
+#ifndef LAUNCH_CORE_LIS3MDL_H
+#define LAUNCH_CORE_LIS3MDL_H
+
+class LIS3MDL {
+public:
+
+private:
+
+};
+
+#endif //LAUNCH_CORE_LIS3MDL_H
