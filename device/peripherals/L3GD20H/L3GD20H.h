ac35caa01070e4dff213629e4ffd89d60244e76f
Aaron Chan <aarchan108@gmail.com>
2022-12-23 22:12:40 -0500

    Added l3gd driver code

---
+++ device/peripherals/L3GD20H/L3GD20H.h
@@ -0,0 +1,18 @@
+/**
+ * Facade for the L3GD20H STM driver using launch-core
+ *
+ * @author Aaron Chan
+ */
+#ifndef LAUNCH_CORE_L3GD20H_H
+#define LAUNCH_CORE_L3GD20H_H
+
+class L3GD20H {
+public:
+    L3GD20H() {};
+
+
+private:
+
+};
+
+#endif //LAUNCH_CORE_L3GD20H_H
