86937016db840978d0be2b6c9737f9055c1fa86a
Aaron Chan <aarchan108@gmail.com>
2022-12-23 22:18:06 -0500

    Fn ptrs

--- device/peripherals/L3GD20H/L3GD20H.h
+++ device/peripherals/L3GD20H/L3GD20H.h
@@ -6,12 +6,83 @@
 #ifndef LAUNCH_CORE_L3GD20H_H
 #define LAUNCH_CORE_L3GD20H_H
 
+#include "device/peripherals/L3GD20H/l3gd20h_reg.h"
+#include "device/I2CDevice.h"
+#include "device/SPIDevice.h"
+#include "sched/macros/resume.h"
+#include "sched/macros/call.h"
+#include "sched/macros/reset.h"
+
+
 class L3GD20H {
 public:
-    L3GD20H() {};
+    L3GD20H(void *pInterface, stmdev_mdelay_ptr delayFptr,
+            SPIDevice *spiDev = nullptr, I2CDevice *i2cDev = nullptr) :
+            device({
+                           .mdelay = delayFptr
+                   }) {
+        mSPI = spiDev;
+        mI2C = i2cDev;
+    };
 
 
 private:
+    static I2CDevice *mI2C;
+    static SPIDevice *mSPI;
+    stmdev_ctx_t device;
+
+    static int32_t i2cRead(void *handle, uint8_t reg, uint8_t *data, uint16_t len) {
+        RESUME();
+
+        I2CAddr_t addr = {
+                .dev_addr = L3GD20H_I2C_ADD_L,
+                .mem_addr = reg,
+                .mem_addr_size = sizeof(uint8_t),
+        };
+
+        RetType ret = CALL(mI2C->read(addr, const_cast<uint8_t *>(data), len));
+
+        RESET();
+        return ret == RET_SUCCESS ? 0 : -1;
+
+    };
+
+    static int32_t i2cWrite(void *handle, uint8_t reg, const uint8_t *data, uint16_t len) {
+        RESUME();
+
+        I2CAddr_t addr = {
+                .dev_addr = L3GD20H_I2C_ADD_L,
+                .mem_addr = reg,
+                .mem_addr_size = sizeof(uint8_t),
+        };
+
+        RetType ret = CALL(mI2C->write(addr, const_cast<uint8_t *>(data), len));
+
+        RESET();
+        return ret == RET_SUCCESS ? 0 : -1;
+    };
+
+    // TODO: Not using SPI, but should figure out how to use regAddr here
+    // Would need to also use cs pin and do both tx and rx
+    static int32_t spiRead(void *handle, uint8_t reg, uint8_t *data, uint16_t len) {
+        RESUME();
+
+        RetType ret = CALL(mSPI->read(const_cast<uint8_t *>(data), len));
+
+        RESET();
+        return ret == RET_SUCCESS ? 0 : -1;
+    };
+
+    static int32_t spiWrite(void *handle, uint8_t reg, const uint8_t *data, uint16_t len) {
+        RESUME();
+
+        RetType ret = CALL(mSPI->read(const_cast<uint8_t *>(data), len));
+
+        RESET();
+        return ret == RET_SUCCESS ? 0 : -1;
+
+    };
+
 
 };
 
