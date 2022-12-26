b18bd9dae47dd8e78cb760abdc2544db7dd6e514
Aaron Chan <aarchan108@gmail.com>
2022-12-26 11:51:08 -0500

    Initialize settings from init func

--- device/peripherals/bmp390/bmp390.h
+++ device/peripherals/bmp390/bmp390.h
@@ -66,9 +66,12 @@ public:
 
 
         int8_t result = bmp3_init(&this->device);
+        if (result != BMP3_OK) return RET_ERROR;
+
+        result = initSettings();
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     // TODO: Provided fn ptrs having issues with scheduler
@@ -96,9 +99,13 @@ public:
 
 
         int8_t result = bmp3_init(&this->device);
+        if (result != BMP3_OK) return RET_ERROR;
+
+        result = initSettings();
+
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType getSensorData(uint8_t sensorComp, struct bmp3_data *data) {
@@ -107,7 +114,7 @@ public:
         int8_t result = bmp3_get_sensor_data(sensorComp, data, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType softReset() {
@@ -116,7 +123,7 @@ public:
         int8_t result = bmp3_soft_reset(&this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
 
@@ -126,7 +133,7 @@ public:
         int8_t result = bmp3_set_regs(regAddress, regData, len, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
 
@@ -136,7 +143,7 @@ public:
         int8_t result = bmp3_get_regs(regAddress, regData, len, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     /*************************************************************************************
@@ -149,7 +156,7 @@ public:
         int8_t result = bmp3_get_fifo_data(fifo, fifoSettings, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType getFifoLength(uint16_t *fifoLength) {
@@ -158,7 +165,7 @@ public:
         int8_t result = bmp3_get_fifo_length(fifoLength, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType getFifoWatermark(uint16_t *watermarkLength) {
@@ -167,7 +174,7 @@ public:
         int8_t result = bmp3_get_fifo_watermark(watermarkLength, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType getStatus(struct bmp3_status *status) {
@@ -176,7 +183,7 @@ public:
         int8_t result = bmp3_get_status(status, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType extractFifoData(struct bmp3_data *data, struct bmp3_fifo_data *fifoData) {
@@ -185,7 +192,7 @@ public:
         int8_t result = bmp3_extract_fifo_data(data, fifoData, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType flushFifo() {
@@ -194,7 +201,7 @@ public:
         int8_t result = bmp3_fifo_flush(&this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     /*************************************************************************************
@@ -207,7 +214,7 @@ public:
         int8_t result = bmp3_set_sensor_settings(desiredSettings, settings, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType setOperatingMode(bmp3_settings *settings) {
@@ -216,7 +223,7 @@ public:
         int8_t result = bmp3_set_op_mode(settings, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
     RetType getPowerMode(uint8_t *opMode) {
@@ -225,7 +232,7 @@ public:
         int8_t result = bmp3_get_op_mode(opMode, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
 
@@ -235,17 +242,17 @@ public:
         int8_t result = bmp3_set_fifo_settings(desiredSettings, fifo_settings, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
+    }
 
 
-        RetType getFifoSettings(struct bmp3_fifo_settings *fifoSettings) {
-            RESUME();
+    RetType getFifoSettings(struct bmp3_fifo_settings *fifoSettings) {
+        RESUME();
 
-            int8_t result = bmp3_get_fifo_settings(fifoSettings, &this->device);
+        int8_t result = bmp3_get_fifo_settings(fifoSettings, &this->device);
 
-            RESET();
-            return result == 0 ? RET_SUCCESS : RET_ERROR;
-        }
+        RESET();
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
 
@@ -256,13 +263,46 @@ public:
         int8_t result = bmp3_set_fifo_watermark(fifoData, fifoSettings, &this->device);
 
         RESET();
-        return result == 0 ? RET_SUCCESS : RET_ERROR;
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
     }
 
 private:
     bmp3_dev device;
     inline static I2CDevice *mI2C;
     inline static SPIDevice *mSPI;
+
+    RetType initSettings() {
+        bmp3_settings settings = {
+                .op_mode = BMP3_MODE_NORMAL,
+                .press_en = BMP3_ENABLE,
+                .temp_en = BMP3_ENABLE,
+                .odr_filter = {
+                        .press_os = BMP3_OVERSAMPLING_2X,
+                        .temp_os = BMP3_OVERSAMPLING_2X,
+                        .iir_filter = BMP3_IIR_FILTER_DISABLE,
+                        .odr = BMP3_ODR_100_HZ
+                },
+                .int_settings = {
+                        .output_mode = BMP3_INT_PIN_PUSH_PULL,
+                        .level = BMP3_INT_PIN_ACTIVE_LOW,
+                        .latch = BMP3_INT_PIN_NON_LATCH,
+                        .drdy_en = BMP3_ENABLE
+                },
+                .adv_settings = {
+                        .i2c_wdt_en = BMP3_DISABLE,
+                        .i2c_wdt_sel = BMP3_DISABLE
+                }
+        };
+
+        uint16_t settingsSel = BMP3_SEL_PRESS_EN | BMP3_SEL_TEMP_EN | BMP3_SEL_PRESS_OS |
+                               BMP3_SEL_TEMP_OS | BMP3_SEL_ODR | BMP3_SEL_DRDY_EN;
+        int result = setSensorSettings(settingsSel, &settings);
+        if (result != BMP3_OK) return RET_ERROR;
+
+        result = setOperatingMode(&settings);
+
+        return result == BMP3_OK ? RET_SUCCESS : RET_ERROR;
+    }
 
     static BMP3_INTF_RET_TYPE i2cRead(uint8_t regAddr, uint8_t *data, uint32_t len, void *intfPtr) {
         RESUME();
@@ -301,7 +341,7 @@ private:
         return RET_SUCCESS;
     };
 
-    // TODO: Not using SPI, but should figure out how to use regAddr here
+// TODO: Not using SPI, but should figure out how to use regAddr here
     static BMP3_INTF_RET_TYPE spiRead(uint8_t regAddr, uint8_t *data, uint32_t len, void *intfPtr) {
         RESUME();
         RetType ret = mSPI->read(data, len);
