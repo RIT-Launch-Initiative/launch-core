698ae6afdc00ed4f2ca918567a05d1fb85f1ee57
Aaron Chan <aarchan108@gmail.com>
2023-01-01 02:08:10 -0500

    Init function

--- device/peripherals/LSM6DSL/LSM6DSL.h
+++ device/peripherals/LSM6DSL/LSM6DSL.h
@@ -14,33 +14,105 @@
 
 class LSM6DSL {
 public:
-    LSM6DSL(I2CDevice* i2CDevice) : mI2C(i2CDevice), xEnabled(false), gEnabled(false) {}
+    LSM6DSL(I2CDevice *i2CDevice) : mI2C(i2CDevice), xEnabled(false), gEnabled(false) {}
 
-    RetType init() {
+    RetType init(uint8_t i2cDevAddr) {
+        RESUME();
+        i2cAddr = {
+                .dev_addr = i2cDevAddr,
+                .mem_addr = LSM6DSL_ACC_GYRO_WHO_AM_I_REG,
+                .mem_addr_size = 1
+        };
+
+        // Enable reg addr automatically incremented during multi byte access with serial intf
+        RetType ret = CALL(
+                writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1,
+                         LSM6DSL_ACC_GYRO_IF_INC_MASK));
+        if (ret != RET_SUCCESS) return ret;
+
+        // Enable BDU
+        ret = CALL(writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_BDU_BLOCK_UPDATE), 1,
+                            LSM6DSL_ACC_GYRO_BDU_MASK));
+        if (ret != RET_SUCCESS) return ret;
+
+        // FIFO Mode Select
+        ret = CALL(
+                writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_FIFO_MODE_BYPASS), 1,
+                         LSM6DSL_ACC_GYRO_FIFO_MODE_MASK));
+        if (ret != RET_SUCCESS) return ret;
+
+        // Output Data Rate Selection
+        ret = CALL(
+                writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_ODR_XL_POWER_DOWN),
+                         1, LSM6DSL_ACC_GYRO_ODR_FIFO_MASK));
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = CALL(setFullScaleX(2.0f));
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = CALL(writeReg(LSM6DSL_ACC_GYRO_CTRL2_G, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_ODR_G_POWER_DOWN), 1,
+                            LSM6DSL_ACC_GYRO_IF_INC_MASK));
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = CALL(setFullScaleG(2000.0f));
+        if (ret != RET_SUCCESS) return ret;
+
+        xLastODR = 104.0f;
+        xEnabled = 0;
+        gLastODR = 104.0f;
+        gEnabled = 0;
+
+        RESET();
+        return RET_SUCCESS;
+    }
+
+    RetType setFullScaleX(float fullScale) {
         RESUME();
 
-        RetType ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
-        if (ret != RET_SUCCESS) return ret;
+        LSM6DSL_ACC_GYRO_FS_XL_t newFs = (fullScale <= 2.0f) ? LSM6DSL_ACC_GYRO_FS_XL_2g : (fullScale <= 4.0f)
+                                                                                           ? LSM6DSL_ACC_GYRO_FS_XL_4g
+                                                                                           : (fullScale <= 8.0f)
+                                                                                             ? LSM6DSL_ACC_GYRO_FS_XL_8g
+                                                                                             : LSM6DSL_ACC_GYRO_FS_XL_16g;
 
-        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_BDU_BLOCK_UPDATE), 1, LSM6DSL_ACC_GYRO_BDU_MASK);
-        if (ret != RET_SUCCESS) return ret;
-
-
-        ret = writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_FIFO_MODE_BYPASS), 1, LSM6DSL_ACC_GYRO_FIFO_MODE_MASK);
-        if (ret != RET_SUCCESS) return ret;
-
-        ret = writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_ODR_XL_POWER_DOWN), 1, LSM6DSL_ACC_GYRO_ODR_FIFO_MASK);
+        RetType ret = CALL(
+                writeReg(LSM6DSL_ACC_GYRO_CTRL1_XL, reinterpret_cast<uint8_t *>(&newFs), 1, LSM6DSL_ACC_GYRO_CTRL1_XL));
         if (ret != RET_SUCCESS) return ret;
 
 
+        RESET();
+        return RET_SUCCESS;
+    }
+
+    RetType setFullScaleG(float fullScale) {
+        RESUME();
+
+        LSM6DSL_ACC_GYRO_FS_G_t newFs;
+
+        if (fullScale <= 125.0f) {
+            writeReg(LSM6DSL_ACC_GYRO_CTRL2_G, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_FS_125_ENABLED), 1,
+                     LSM6DSL_ACC_GYRO_FS_125_MASK)
+        } else {
+            newFs = (fullScale <= 245.0f) ? LSM6DSL_ACC_GYRO_FS_G_245dps
+                                          : (fullScale <= 500.0f) ? LSM6DSL_ACC_GYRO_FS_G_500dps
+                                                                  : (fullScale <= 1000.0f)
+                                                                    ? LSM6DSL_ACC_GYRO_FS_G_1000dps
+                                                                    : LSM6DSL_ACC_GYRO_FS_G_2000dps;
+            RetType ret = writeReg(LSM6DSL_ACC_GYRO_CTRL2_G,
+                                   reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_FS_125_DISABLED), 1,
+                                   LSM6DSL_ACC_GYRO_FS_125_MASK);
+            if (ret != RET_SUCCESS) return ret;
+
+            ret = writeReg(LSM6DSL_ACC_GYRO_CTRL2_G,
+                           reinterpret_cast<uint8_t *>(newFs), 1,
+                           LSM6DSL_ACC_GYRO_FS_G_MASK);
+            if (ret != RET_SUCCESS) return ret;
+
+        }
 
 
-        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
+        RetType ret = CALL(writeReg(LSM6DSL_ACC_GYRO_CTRL1_XL, &newFs, 1));
         if (ret != RET_SUCCESS) return ret;
-
-        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
-        if (ret != RET_SUCCESS) return ret;
-
 
         RESET();
         return RET_SUCCESS;
@@ -49,11 +121,16 @@ public:
 private:
     I2CDevice *mI2C;
     I2CAddr_t i2cAddr;
+    float xLastODR;
+    float gLastODR;
     bool xEnabled;
     bool gEnabled;
 
+
     RetType readReg(uint8_t reg, uint8_t *buff, size_t len, uint8_t mask) {
         RESUME();
+
+        i2cAddr.mem_addr = reg;
 
         RetType ret = CALL(mI2C->read(i2cAddr, buff, len));
         if (ret != RET_SUCCESS) return ret;
@@ -81,7 +158,6 @@ private:
         RESET();
         return RET_SUCCESS;
     }
-
 
 
 };
