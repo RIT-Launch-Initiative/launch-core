8c3834046872970d0926e4f068b5756a2d994ed8
Aaron Chan <aarchan108@gmail.com>
2023-01-01 01:45:05 -0500

    Read and Write register functions. Some init fn calls

--- device/peripherals/LSM6DSL/LSM6DSL.h
+++ device/peripherals/LSM6DSL/LSM6DSL.h
@@ -9,28 +9,81 @@
 
 #include <stdint.h>
 #include "device/I2CDevice.h"
+#include "sched/macros.h"
+#include "device/peripherals/LSM6DSL/LSM6DSL_Driver.h"
 
 class LSM6DSL {
 public:
-    LSM6DSL(I2CDevice* i2CDevice) : mI2C(i2CDevice) {
+    LSM6DSL(I2CDevice* i2CDevice) : mI2C(i2CDevice), xEnabled(false), gEnabled(false) {}
 
+    RetType init() {
+        RESUME();
+
+        RetType ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_BDU_BLOCK_UPDATE), 1, LSM6DSL_ACC_GYRO_BDU_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+
+        ret = writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_FIFO_MODE_BYPASS), 1, LSM6DSL_ACC_GYRO_FIFO_MODE_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = writeReg(LSM6DSL_ACC_GYRO_FIFO_CTRL5, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_ODR_XL_POWER_DOWN), 1, LSM6DSL_ACC_GYRO_ODR_FIFO_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+
+
+
+        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+        ret = writeReg(LSM6DSL_ACC_GYRO_CTRL3_C, reinterpret_cast<uint8_t *>(LSM6DSL_ACC_GYRO_IF_INC_ENABLED), 1, LSM6DSL_ACC_GYRO_IF_INC_MASK);
+        if (ret != RET_SUCCESS) return ret;
+
+
+        RESET();
+        return RET_SUCCESS;
     }
 
 private:
     I2CDevice *mI2C;
+    I2CAddr_t i2cAddr;
+    bool xEnabled;
+    bool gEnabled;
+
+    RetType readReg(uint8_t reg, uint8_t *buff, size_t len, uint8_t mask) {
+        RESUME();
+
+        RetType ret = CALL(mI2C->read(i2cAddr, buff, len));
+        if (ret != RET_SUCCESS) return ret;
+
+        *buff &= mask;
+
+        RESET();
+        return RET_SUCCESS;
+    }
+
+    RetType writeReg(uint8_t reg, uint8_t *buff, size_t len, uint8_t mask) {
+        RESUME();
+
+        uint8_t value;
+        i2cAddr.mem_addr = reg;
+
+        RetType ret = CALL(mI2C->read(i2cAddr, &value, len));
+        if (ret != RET_SUCCESS) return ret;
+
+        value &= ~mask;
+        value |= *buff;
+
+        ret = CALL(mI2C->write(i2cAddr, &value, len));
+
+        RESET();
+        return RET_SUCCESS;
+    }
+
+
 
 };
-
-/* Imported function prototypes ----------------------------------------------*/
-uint8_t LSM6DSL_IO_Write(void *handle, uint8_t WriteAddr, uint8_t *pBuffer, uint16_t nBytesToWrite) {
-
-    return 0;
-}
-
-uint8_t LSM6DSL_IO_Read(void *handle, uint8_t ReadAddr, uint8_t *pBuffer, uint16_t nBytesToRead) {
-
-
-    return 0;
-}
 
 #endif //LAUNCH_CORE_LSM6DSL_H
