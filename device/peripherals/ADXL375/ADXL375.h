964952861bbd3de2c7148b8ffe31b0a325de22b2
Aaron Chan <aarchan108@gmail.com>
2023-03-07 21:10:00 -0500

    Self test function

--- device/peripherals/ADXL375/ADXL375.h
+++ device/peripherals/ADXL375/ADXL375.h
@@ -77,7 +77,7 @@ public:
         if (ret != RET_SUCCESS) return ret;
         if (id != 0xE5) return RET_ERROR;
 
-        ret = CALL(setDataRate(ADXL375_DR_6HZ25));
+        ret = CALL(setDataRateAndLowPower(ADXL375_DR_100HZ, false));
         if (ret != RET_SUCCESS) return ret;
 
         ret = CALL(setOperatingMode(ADXL375_MEASURING_MODE));
@@ -214,24 +214,27 @@ public:
         RESUME();
 
         i2cAddr.mem_addr = ADXL375_POWER_CTL;
-        RetType ret = CALL(m_i2c.write(i2cAddr, reinterpret_cast<uint8_t*>(0x08), 1));
+        RetType ret = CALL(m_i2c.write(i2cAddr, reinterpret_cast<uint8_t *>(0x08), 1));
 
         RESET();
         return ret;
     }
 
-    RetType setDataRate(ADXL375_DATA_RATE dataRate) {
+    RetType setDataRateAndLowPower(ADXL375_DATA_RATE dataRate, bool lowPower) {
         RESUME();
         i2cAddr.mem_addr = ADXL375_REG_BW_RATE;
-        RetType ret = CALL(m_i2c.write(i2cAddr, reinterpret_cast<uint8_t*>(&dataRate), 1));
+        static uint8_t rate = static_cast<uint8_t>(dataRate);
+        if (lowPower) rate |= 0x8;
+
+        RetType ret = CALL(m_i2c.write(i2cAddr, &rate, 1));
         RESET();
         return ret;
     }
 
-    RetType setOperatingMode(ADXL375_OP_MODE opMode){
+    RetType setOperatingMode(ADXL375_OP_MODE opMode) {
         RESUME();
         i2cAddr.mem_addr = ADXL375_POWER_CTL;
-        RetType ret = CALL(m_i2c.write(i2cAddr, reinterpret_cast<uint8_t*>(&opMode), 1));
+        RetType ret = CALL(m_i2c.write(i2cAddr, reinterpret_cast<uint8_t *>(&opMode), 1));
         RESET();
         return ret;
     }
@@ -239,9 +242,12 @@ public:
     RetType setOffset(int16_t xOffset, int16_t yOffset, int16_t zOffset) {
         RESUME();
 
-        static uint8_t xOffBuff[2] = {static_cast<uint8_t>(xOffset & 0xFF), static_cast<uint8_t>((xOffset >> 8) & 0xFF)};
-        static uint8_t yOffBuff[2] = {static_cast<uint8_t>(yOffset & 0xFF), static_cast<uint8_t>((yOffset >> 8) & 0xFF)};
-        static uint8_t zOffBuff[2] = {static_cast<uint8_t>(zOffset & 0xFF), static_cast<uint8_t>((zOffset >> 8) & 0xFF)};
+        static uint8_t xOffBuff[2] = {static_cast<uint8_t>(xOffset & 0xFF),
+                                      static_cast<uint8_t>((xOffset >> 8) & 0xFF)};
+        static uint8_t yOffBuff[2] = {static_cast<uint8_t>(yOffset & 0xFF),
+                                      static_cast<uint8_t>((yOffset >> 8) & 0xFF)};
+        static uint8_t zOffBuff[2] = {static_cast<uint8_t>(zOffset & 0xFF),
+                                      static_cast<uint8_t>((zOffset >> 8) & 0xFF)};
 
 
         i2cAddr.mem_addr = offsetXReg;
@@ -263,11 +269,50 @@ public:
 
 private:
     I2CDevice &m_i2c;
-    I2CAddr_t i2cAddr {
-        .dev_addr = ADXL375_DEV_ADDR_SEC << 1,
-        .mem_addr = 0x00,
-        .mem_addr_size = 1
+    I2CAddr_t i2cAddr{
+            .dev_addr = ADXL375_DEV_ADDR_SEC << 1,
+            .mem_addr = 0x00,
+            .mem_addr_size = 1
     };
+
+    RetType selfTest(int16_t *xAvg, int16_t *yAvg, int16_t *zAvg) {
+        RESUME();
+
+        static uint8_t dataFormat = 0;
+        static int16_t x = 0;
+        static int16_t y = 0;
+        static int16_t z = 0;
+
+        i2cAddr.mem_addr = ADXL375_REG_DATA_FORMAT;
+        RetType ret = CALL(m_i2c.read(i2cAddr, &dataFormat, 1));
+        if (ret != RET_SUCCESS) return ret;
+
+        dataFormat |= (1 << 7);
+        ret = CALL(m_i2c.write(i2cAddr, &dataFormat, 1));
+        if (ret != RET_SUCCESS) return ret;
+
+        for (int i = 0; i < 10; i++) {
+            ret = CALL(readXYZ(&x, &y, &z));
+            if (ret != RET_SUCCESS) return ret;
+
+            *xAvg += x;
+            *yAvg += y;
+            *zAvg += z;
+        }
+
+        *xAvg /= 10;
+        *yAvg /= 10;
+        *zAvg /= 10;
+
+        dataFormat &= ~(1 << 7);
+        ret = CALL(m_i2c.write(i2cAddr, &dataFormat, 1));
+        if (ret != RET_SUCCESS) return ret;
+
+        RESET();
+        return RET_SUCCESS;
+    }
+
+
 };
 
 
