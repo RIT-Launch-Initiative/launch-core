d037aa5606369fda7025dbb97a1fd1b98c57cec1
Aaron Chan <aarchan108@gmail.com>
2023-03-15 15:03:43 -0400

    Little fs init function

--- filesystem/LittleFS/LittleFS.h
+++ filesystem/LittleFS/LittleFS.h
@@ -14,8 +14,46 @@
 
 class LittleFS : public FileSystem {
 public:
+    LittleFS(BlockDevice& block_device) : m_block_device(block_device) {}
+
+    RetType init() {
+        RESUME();
+
+        lfs_cfg.context = &m_block_device;
+        lfs_cfg.read_size = m_block_device.getBlockSize();
+        lfs_cfg.prog_size = m_block_device.getBlockSize();
+        lfs_cfg.block_size = m_block_device.getBlockSize();
+        lfs_cfg.block_count = m_block_device.getNumBlocks();
+        lfs_cfg.block_cycles = 500;
+        lfs_cfg.cache_size = 512;
+        lfs_cfg.lookahead_size = 512;
+        lfs_cfg.name_max = 255;
+        lfs_cfg.file_max = 0;
+        lfs_cfg.attr_max = 0;
+
+        int err = lfs_mount(&lfs, &lfs_cfg);
+        if (err) {
+            err = lfs_format(&lfs, &lfs_cfg);
+            if (err) {
+                RESET();
+                return RET_ERROR;
+            }
+
+            err = lfs_mount(&lfs, &lfs_cfg);
+            if (err) {
+                RESET();
+                return RET_ERROR;
+            }
+        }
+
+        RESET();
+        return RET_SUCCESS;
+    }
+
     RetType open(const char *filename, int *fd, bool *new_file = NULL) {
         RESUME();
+
+
 
         RESET();
         return RET_SUCCESS;
@@ -53,7 +91,8 @@ public:
 
 private:
     lfs_t lfs;
-    BlockDevice* m_block_device;
+    lfs_config lfs_cfg;
+    BlockDevice& m_block_device;
 
 };
 
