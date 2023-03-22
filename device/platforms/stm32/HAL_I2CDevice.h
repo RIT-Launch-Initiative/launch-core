e9220805d629e7ce7bf4d965484b2390b0a23a7c
Aaron Chan <aarchan108@gmail.com>
2023-03-22 12:47:50 -0400

    Set m_blocked to -1 for I2C after an error

--- device/platforms/stm32/HAL_I2CDevice.h
+++ device/platforms/stm32/HAL_I2CDevice.h
@@ -101,8 +101,8 @@ public:
 
         // start the transfer
         if (HAL_OK != HAL_I2C_Master_Transmit_IT(m_i2c, addr.dev_addr, buff, len)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
-
             RESET();
             return RET_ERROR;
         }
@@ -164,6 +164,7 @@ public:
         // start the transfer
         if (HAL_OK != HAL_I2C_Mem_Write_IT(m_i2c, addr.dev_addr, addr.mem_addr,
                                                addr.mem_addr_size, buff, len)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
             RESET();
             return RET_ERROR;
@@ -226,6 +227,7 @@ public:
         // start the transfer
         if (HAL_OK != HAL_I2C_Master_Receive_IT(m_i2c, addr.dev_addr,
                                                             buff, len)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
 
             RESET();
@@ -290,6 +292,7 @@ public:
         // start the transfer
         if (HAL_OK != HAL_I2C_Mem_Read_IT(m_i2c, addr.dev_addr, addr.mem_addr,
                                           addr.mem_addr_size, buff, len)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
 
             RESET();
@@ -355,6 +358,7 @@ public:
 
         // start the transfer
         if (HAL_OK != HAL_I2C_Master_Transmit_IT(m_i2c, addr.dev_addr, buff, inLen)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
             RESET();
             return RET_ERROR;
@@ -378,8 +382,8 @@ public:
         }
 
         if (HAL_OK != HAL_I2C_Master_Receive_IT(m_i2c, addr.dev_addr, buff, outLen)) {
+            m_blocked = -1;
             CALL(m_lock.acquire());
-
             RESET();
             return RET_ERROR;
         }
