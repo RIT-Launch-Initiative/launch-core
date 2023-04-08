f48df63d527160f405a3c0f905b759dc991dbb14
Aaron Chan <aarchan108@gmail.com>
2023-04-08 00:26:57 -0400

    Modify conversion to use pointers instead of indexing

--- utils/test/data_encode_test.cpp
+++ utils/test/data_encode_test.cpp
@@ -19,13 +19,13 @@ typedef struct {
 // NOTE: *buffer should point to the start of where you want to write the data into
 // Make sure you do not overwrite if you are writing multiple structs into the same buffer
 // Each struct and encode/decode functions can go into its related class. Struct at the top and static functions at the bottom outside of the class
-void example_encode(example_struct_t *telem, uint8_t *buffer) {
+constexpr void example_encode(example_struct_t *telem, uint8_t *buffer) {
     uint16_to_uint8(telem->identifier, buffer);
     int16_to_uint8(telem->some_flight_data, buffer + 2);
     *(buffer + 4) = telem->some_other_flight_data;
 }
 
-void example_decode(example_struct_t *telem, uint8_t *buffer) {
+constexpr void example_decode(example_struct_t *telem, uint8_t *buffer) {
     telem->identifier = uint8_to_uint16(buffer);
     telem->some_flight_data = uint8_to_int16(buffer + 2);
     telem->some_other_flight_data = *(buffer + 4);
