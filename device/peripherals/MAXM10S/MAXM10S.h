f759e0a55d4fc534ac12caa27a3c6dfa26b653f1
Nate Aquino <naquino14@outlook.com>
2023-04-08 12:47:23 -0400

    Added enum of relevant registers

--- device/peripherals/MAXM10S/MAXM10S.h
+++ device/peripherals/MAXM10S/MAXM10S.h
@@ -1,13 +1,17 @@
 /**
  * @file MAXM10S.h
  * @brief Platform Independent Driver for the MAX-M10S GPS module
- *
+ * @details This is the I2C Implementation.
+ *          UART and PIO are also supported but are NYI.
+ *          Im not even sure if we are going to need them but the option
+ *          is there. 🤯
  * @author Nate Aquino
  */
 #ifndef LAUNCH_CORE_MAXM10S_H
 #define LAUNCH_CORE_MAXM10S_H
 
-#pragma region Includes
+/// @note Documentation for this device lives here:
+/// @link https://content.u-blox.com/sites/default/files/MAX-M10S_IntegrationManual_UBX-20053088.pdf
 
 #include "device/I2CDevice.h"
 #include "return.h"
@@ -15,9 +19,21 @@
 #include "sched/macros/reset.h"
 #include "sched/macros/resume.h"
 
-#pragma endregion
-
+/* The MAXM10S (default) I2C address (8 bits) */
 #define MAXM10S_I2C_ADDR 0x42
+
+/**
+ * @brief The MAXM10S Registers
+ *
+ */
+enum MAXM10S_REG {
+    /* The hight byte of the amount of data available from the sensor */
+    BYTE_COUNT_HIGH = 0xFD,
+    /* The low byte of the amount of data available from the sensor */
+    BYTE_COUNT_LOW = 0xFE,
+    /* The data stream register */
+    DATA_STREAM = 0xFF
+};
 
 /**
  * @brief Platform Independent Driver for the MAX-M10S GPS module
