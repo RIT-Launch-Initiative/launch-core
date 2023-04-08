50bfeb9eace96d7ee3c86e1b1e9148ef5802acd6
Aaron Chan <aarchan108@gmail.com>
2023-04-07 23:48:22 -0400

    Stubbed data_encode_test for Brian and Torin

---
+++ utils/test/data_encode_test.cpp
@@ -0,0 +1,74 @@
+/**
+* Tests to ensure writing and reading data will be correct
+ *
+ * @author Aaron Chan
+*/
+
+// TODO: Include relevant headers here
+#include <cstdint>
+#include <utils/conversion.h>
+
+// TODO: Remove this struct when you have your own and know what to do
+typedef struct {
+    uint16_t identifier; // TODO: Port will be the identifier https://wiki.rit.edu/display/ritlaunch/Backplane+Telemetry+Port+Mappings
+    int16_t some_flight_data;
+    uint8_t some_other_flight_data;
+} example_struct_t;
+
+void example_encode(example_struct_t *telem, uint8_t *buffer) {
+    uint16_to_uint8(telem->identifier, buffer);
+    int16_to_uint8(telem->some_flight_data, buffer + 2);
+    *(buffer + 4) = telem->some_other_flight_data;
+}
+
+void example_decode(example_struct_t *telem, uint8_t *buffer) {
+    telem->identifier = uint8_to_uint16(buffer);
+    telem->some_flight_data = uint8_to_int16(buffer + 2);
+    telem->some_other_flight_data = *(buffer + 4);
+}
+
+// TODO: Add tests that encode a single struct each
+
+// TODO: Remove when understood
+// constexpr allows the compiler to evaluate the function at compile time
+constexpr bool encode_example_struct_test() {
+    uint8_t buffer[5] = {};
+    example_struct_t example_struct = {10000, -10, 200};
+
+    example_encode(&example_struct, buffer);
+
+    return buffer[0] == 39 && buffer[1] == 16 && buffer[2] == 246 && buffer[3] == 255 && buffer[4] == 200;
+}
+
+// TODO: Add tests that can decode a uint8_t buffer and convert it into a struct
+constexpr bool decode_example_struct_test() {
+    uint8_t buffer[5] = {39, 16, 246, 255, 200};
+    example_struct_t example_struct = {};
+
+    example_decode(&example_struct, buffer);
+
+    return example_struct.identifier == 10000 && example_struct.some_flight_data == -10 &&
+           example_struct.some_other_flight_data == 200;
+}
+
+// TODO: Tests where you encode a struct and then decode it and compare the values
+constexpr bool encode_decode_example_struct_test() {
+    uint8_t buffer[5] = {};
+    example_struct_t expected = {10000, -10, 200};
+    example_struct_t actual = {};
+
+    example_encode(&expected, buffer);
+    example_decode(&actual, buffer);
+
+    return expected.identifier == actual.identifier &&
+           expected.some_flight_data == actual.some_flight_data &&
+           expected.some_other_flight_data == actual.some_other_flight_data;
+}
+
+
+int main() {
+    // TODO: Remove when understood. static_assert will cause a compile time error if the expression is false
+    static_assert(encode_example_struct_test(), "Example test failed");
+}
+
+
