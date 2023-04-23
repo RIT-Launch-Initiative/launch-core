9c88c91550e342fc6e61c8079a544b640bb80495
Aaron Chan <aarchan108@gmail.com>
2023-04-23 02:47:23 -0400

    Fix initializing UDP stack and EthLayer

--- net/stack/IPv4UDP/IPv4UDPStack.h
+++ net/stack/IPv4UDP/IPv4UDPStack.h
@@ -48,18 +48,18 @@ public:
     IPv4UDPStack(uint8_t a, uint8_t b, uint8_t c, uint8_t d,
                  uint8_t e, uint8_t f, uint8_t g, uint8_t h,
                  NetworkLayer& dev)
-                                        : m_dev(dev),
-                                          m_udp(m_ip),
+                                        : m_udp(m_ip),
                                           m_ip(),
                                           m_arp(m_eth),
                                           m_eth(SimpleArpLayer::FIXED_MAC_1,
                                                 SimpleArpLayer::FIXED_MAC_2,
                                                 a, b, c, d,
-                                                m_dev,
+                                                dev,
                                                 m_arp,
                                                 eth::IPV4_PROTO,
                                                 true),
                                           m_lo(),
+                                          m_dev(dev),
                                           m_socks() {
         ipv4::IPv4Address(a, b, c, d, &m_ipAddr);
         ipv4::IPv4Address(e, f, g, h, &m_ipSubnet);
