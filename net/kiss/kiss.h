3b8e4006a564bf082a76301a65357ff589e8ae08
Aaron Chan <aarchan108@gmail.com>
2023-07-08 19:38:50 -0600

    I forgot how to define enums. Also added some setters for port and command

--- net/kiss/kiss.h
+++ net/kiss/kiss.h
@@ -7,20 +7,20 @@
 
 namespace kiss {
     typedef enum {
-        const uint8_t FRAME_END = 0xC0;
-        const uint8_t FRAME_ESC = 0xDB;
-        const uint8_T TRANS_FRAME_END = 0xDC;
-        const uint8_t TRANS_FRAME_ESC = 0xDD;
+        FRAME_END = 0xC0;
+        FRAME_ESC = 0xDB;
+        TRANS_FRAME_END = 0xDC;
+        TRANS_FRAME_ESC = 0xDD;
     } special_chars_t;
 
     typedef enum {
-        const uint8_t TX_DELAY_CMD = 0x01;
-        const uint8_t PERSISTENCE_CMD = 0x02;
-        const uint8_t SLOT_TIME_CMD = 0x03;
-        const uint8_t TX_TAIL_CMD = 0x04;
-        const uint8_t FULL_DUPLEX_CMD = 0x05;
-        const uint8_t SET_HW_CMD = 0x06;
-        const uint8_t RETURN_CMD = 0xFF;
+        TX_DELAY_CMD = 0x01;
+        PERSISTENCE_CMD = 0x02;
+        SLOT_TIME_CMD = 0x03;
+        TX_TAIL_CMD = 0x04;
+        FULL_DUPLEX_CMD = 0x05;
+        SET_HW_CMD = 0x06;
+        RETURN_CMD = 0xFF;
     } commands_t;
 
     typedef struct {
@@ -38,9 +38,15 @@ public:
     };
 
     void set_port(uint8_t port) {
-        Packet::
+        m_header->port_and_command |= port << 4;
+    }
 
+    void set_command(uint8_t command) {
+        m_header->port_and_command |= command;
+    }
 
+    void set_port_and_command(uint8_t port, uint8_t command) {
+        m_header->port_and_command = port << 4 | command;
     }
 
 
