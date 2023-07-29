0d2edf61fac7a5f8ae3d2b7566c5d6c64c213419
Aaron Chan <aarchan108@gmail.com>
2023-07-28 23:53:41 -0600

    Link msg stub

---
+++ common/LinkMsg.h
@@ -0,0 +1,23 @@
+/**
+ * Wrapper for constructing payloads between flight computers and ground equipment
+ *
+ * @author Aaron Chan
+ */
+#ifndef LAUNCH_CORE_LINKMSG_H
+#define LAUNCH_CORE_LINKMSG_H
+
+#include <stdint.h>
+
+class LinkMsg {
+public:
+    LinkMsg() {};
+
+    // TODO: Design the structure for link messages
+
+private:
+    uint8_t data[64];
+
+};
+
+
+#endif //LAUNCH_CORE_LINKMSG_H
