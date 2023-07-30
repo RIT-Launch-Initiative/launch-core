3555d86d644c60152939c3f60744622d5d2f4f9f
Aaron Chan <aarchan108@gmail.com>
2023-07-29 21:36:42 -0600

    Fix includes to use old scheduler again

--- return.h
+++ return.h
@@ -1,11 +1,20 @@
 #ifndef RETURN_H
 #define RETURN_H
 
-/// @brief return codes
+// return codes
+
 typedef enum {
     RET_SUCCESS = 0,        // success
     RET_ERROR,              // some error
-    RET_YIELD               // not done yet, yield back to the scheduler
+    RET_BLOCKED,            // something blocked
+    RET_SLEEP,              // something slept
+    RET_YIELD               // something yielded
 } RetType;
+
+// if RET_BLOCKED, RET_SLEEP, or RET_YIELD are ever returned, the task must return as fast
+// as possible because something called 'sched_block', 'sched_sleep', or wants to return
+
+// only return either code if you called one of those functions and want to
+// return control to the scheduler as fast as possible
 
 #endif
--- sched/macros/test/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ ../../sched.cpp ../macros.cpp test1.cpp $1 -I../../.. -o test1 -ggdb
-g++ ../../sched.cpp ../macros.cpp test2.cpp $1 -I../../.. -o test2 -ggdb
