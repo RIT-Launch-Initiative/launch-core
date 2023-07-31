0b2c73822afcd1eea4a61a2ebe16473af65571bb
Aaron Chan <aarchan108@gmail.com>
2023-07-30 21:12:27 -0600

    Transmit functions for TFTP

--- net/tftp/tftp.h
+++ net/tftp/tftp.h
@@ -3,28 +3,164 @@
  *
  * @author Aaron Chan
 */
+#include <stdint.h>
+#include <string.h>
 
+#include "sched/macros.h"
+#include "net/common.h"
 #include "net/network_layer/NetworkLayer.h"
 
 namespace tftp {
+    static constexpr uint16_t TFTP_PORT = 69;
+
+    typedef enum {
+        READ_REQUEST_OP = 1,
+        WRITE_REQUEST_OP = 2,
+        DATA_OP = 3,
+        ACK_OP = 4,
+        ERROR_OP = 5
+    } TFTP_OPCODE_T;
+
+    typedef enum {
+        NETASCII_MODE = 0,
+        OCTET_MODE,
+        MAIL_MODE
+    } TFTP_MODE_T;
+
+    typedef struct {
+        uint16_t opcode;
+        uint8_t *filename;
+        uint8_t *mode; // "netascii" "octet" or "mail"
+    } TFTP_HEADER_T;
+
+
+
 class TFTP : public NetworkLayer {
 public:
-    TFTP() : NetworkLayer() {}
+
+
+    TFTP(NetworkLayer &layer) : NetworkLayer(), m_m_lowerLayer(&layer) {}
 
     RetType receive(Packet &packet, netinfo_t &info, NetworkLayer *caller) override {
         return RET_ERROR;
     }
 
     RetType transmit(Packet &packet, netinfo_t &info, NetworkLayer *caller) override {
-        return RET_ERROR;
+        RESUME();
+
+        if (lock) {
+            RESET();
+            return RET_ERROR;
+        }
+
+        lock = true;
+
+        TFTP_HEADER_T *header = packet.allocate_header<TFTP_HEADER_T>();
+        if (header == nullptr) {
+            RESET();
+            return RET_ERROR;
+        }
+
+        if (!setHeader(header)) {
+            RESET();
+            return RET_ERROR;
+        }
+        
+        RetType ret = CALL(m_lowerLayer->transmit(packet, info, this));
+
+        RESET();
+        return ret;
     }
 
     RetType transmit2(Packet &packet, netinfo_t &info, NetworkLayer *caller) override {
-        return RET_ERROR;
+        RESUME();
+
+        if (lock) {
+            RESET();
+            return RET_ERROR;
+        }
+
+        lock = true;
+
+        TFTP_HEADER_T *header = packet.allocate_header<TFTP_HEADER_T>();
+        if (header == nullptr) {
+            RESET();
+            return RET_ERROR;
+        }
+
+        if (!setHeader(header)) {
+            RESET();
+            return RET_ERROR;
+        }
+        
+        RetType ret = CALL(m_lowerLayer->transmit2(packet, info, this));
+        
+
+        lock = false;
+        RESET();
+        return ret;
     }
 
-private:
+    void setOpcode(TFTP_OPCODE_T opcode) {
+        this->m_currentOpcode = opcode;
+    }
 
+    void setFilename(const char* filename, size_t len) {
+        memcpy(m_currentFilename, filename, len);
+
+        if (0 != *(m_currentFilename + len) && len + 1 < 256) {
+            *(m_currentFilename + len + 1) = 0;
+            len += 1;
+        } else { // Just guarantee null termination if we can't fit the null
+            *(m_currentFilename + len) = 0;
+        }
+    }
+
+    void setMode(TFTP_MODE_T mode) {
+        switch (mode) {
+            case NETASCII_MODE:
+                memcpy(m_currentMode, "netascii", 8);
+                *(m_currentMode + 8) = 0;
+                m_currentModeLen = 9;
+                break;
+            case OCTET_MODE:
+                memcpy(m_currentMode, "octet", 5);
+                *(m_currentMode + 5) = 0;
+                m_currentModeLen = 6;
+                break;
+            case MAIL_MODE:
+                memcpy(m_currentMode, "mail", 4);
+                *(m_currentMode + 4) = 0;
+                m_currentModeLen = 5;
+                break;
+        }
+    }
+    
+private:
+    const NetworkLayer *m_lowerLayer;
+    
+    TFTP_OPCODE_T m_currentOpcode = READ_REQUEST_OP;
+    
+    uint8_t m_currentFilename[256] = "";
+    uint8_t m_currentFilenameLen = 0;
+
+    uint8_t m_currentMode[9] = "";
+    uint8_t m_currentModeLen = 0;
+
+    bool lock = false; // Avoid setting headers while transmitting
+
+
+    bool setHeader(TFTP_HEADER_T *header) {
+        if (0 == m_currentFilenameLen || 0 == m_currentModeLen) {
+            return false;
+        }
+
+        header->opcode = hton16(m_currentOpcode);
+        header->filename = m_currentFilename;
+        header->mode = m_currentMode;
+
+        return true;
+    }
 
 };
 }
