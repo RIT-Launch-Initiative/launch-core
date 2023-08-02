779f7cb2314fb7a1553d80a5e70961952d364402
Aaron Chan <aarchan108@gmail.com>
2023-08-02 12:56:47 -0600

    Remove static vars in ADXL375 class

--- device/peripherals/ADXL375/ADXL375.h
+++ device/peripherals/ADXL375/ADXL375.h
@@ -1,7 +1,7 @@
 /**
  * ADXL375 Accelerometer Driver
  *
- * @author Aaron Chan and Akhil D
+ * @author Aaron Chan
  */
 
 #ifndef LAUNCH_CORE_ADXL375_H
@@ -179,10 +179,9 @@ public:
 
     RetType readAxis(int16_t *axis, ADXL375_REG axisReg) {
         RESUME();
-        static uint8_t data[2];
 
-        RetType ret = CALL(readReg(axisReg, data, 2));
-        *axis = ((data[1] << 8) | data[2]) * -1;
+        RetType ret = CALL(readReg(axisReg, m_buff, 2));
+        *axis = ((m_buff[0] << 8) | m_buff[1]) * -1;
 
         RESET();
         return ret;
@@ -191,8 +190,8 @@ public:
     RetType wakeup() {
         RESUME();
 
-        static uint8_t val = 0x08;
-        RetType ret = CALL(writeReg(ADXL375_POWER_CTL, &val));
+        m_buff[0] = 0x08;
+        RetType ret = CALL(writeReg(ADXL375_POWER_CTL, m_buff, 1));
 
         RESET();
         return ret;
@@ -200,24 +199,21 @@ public:
 
     RetType setDataRateAndLowPower(ADXL375_DATA_RATE dataRate, bool lowPower) {
         RESUME();
-        i2cAddr.mem_addr = ADXL375_REG_BW_RATE;
-        static uint8_t rate = static_cast<uint8_t>(dataRate);
 
-        if (lowPower) rate |= 0x8;
+        m_buff[0] = dataRate;
+        if (lowPower) m_buff[0] |= 0x8;
 
-        RetType ret = CALL(writeReg(ADXL375_REG_BW_RATE, &rate));
+        RetType ret = CALL(writeReg(ADXL375_REG_BW_RATE, m_buff, 1));
+
         RESET();
-
         return ret;
     }
 
     RetType setOperatingMode(ADXL375_OP_MODE opMode) {
         RESUME();
 
-        static uint8_t operating_mode;
-        operating_mode = static_cast<uint8_t>(opMode);
-
-        RetType ret = CALL(writeReg(ADXL375_POWER_CTL, &operating_mode));
+        m_buff[0] = opMode;
+        RetType ret = CALL(writeReg(ADXL375_POWER_CTL, m_buff, 1));
 
         RESET();
         return ret;
@@ -226,25 +222,33 @@ public:
     RetType setOffset(int16_t xOffset, int16_t yOffset, int16_t zOffset) {
         RESUME();
 
-        static uint8_t xOffBuff[2] = {static_cast<uint8_t>(xOffset & 0xFF),
-                                      static_cast<uint8_t>((xOffset >> 8) & 0xFF)};
-        static uint8_t yOffBuff[2] = {static_cast<uint8_t>(yOffset & 0xFF),
-                                      static_cast<uint8_t>((yOffset >> 8) & 0xFF)};
-        static uint8_t zOffBuff[2] = {static_cast<uint8_t>(zOffset & 0xFF),
-                                      static_cast<uint8_t>((zOffset >> 8) & 0xFF)};
+        m_buff[0] = static_cast<uint8_t>(xOffset & 0xFF);
+        m_buff[1] = static_cast<uint8_t>((xOffset >> 8) & 0xFF);
 
-        RetType ret = CALL(writeReg(offsetXReg, xOffBuff, 2));
-        if (RET_SUCCESS != ret) {
-            RESET();
-            return ret;
-        }
-        ret = CALL(writeReg(offsetYReg, yOffBuff, 2));
-        if (RET_SUCCESS != ret) {
-            RESET();
-            return ret;
-        }
+        // Y Offset
+        m_buff[2] = static_cast<uint8_t>(yOffset & 0xFF);
+        m_buff[3] = static_cast<uint8_t>((yOffset >> 8) & 0xFF);
 
-        ret = CALL(writeReg(offsetZReg, zOffBuff, 2));
+        // Z Offset
+        m_buff[4] = static_cast<uint8_t>(zOffset & 0xFF);
+        m_buff[5] = static_cast<uint8_t>((zOffset >> 8) & 0xFF);
+
+        i2cAddr.mem_addr_size = 2; // TODO: Figure out if we can just make this one call and address sizing
+        RetType ret = CALL(writeReg(offsetXReg, m_buff, 2));
+        i2cAddr.mem_addr_size = 1;
+
+//        RetType ret = CALL(writeReg(offsetXReg, xOffBuff, 2));
+//        if (RET_SUCCESS != ret) {
+//            RESET();
+//            return ret;
+//        }
+//        ret = CALL(writeReg(offsetYReg, yOffBuff, 2));
+//        if (RET_SUCCESS != ret) {
+//            RESET();
+//            return ret;
+//        }
+//
+//        ret = CALL(writeReg(offsetZReg, zOffBuff, 2));
 
         RESET();
         return ret;
