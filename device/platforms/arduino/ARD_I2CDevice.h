d83b25c716bf4e2e4bfb25d3e1be5126e29ddf81
Chloe Clark <etc1444@rit.edu>
2023-08-13 12:03:16 -0400

    Added more I2C functions

--- device/platforms/arduino/ARD_I2CDevice.h
+++ device/platforms/arduino/ARD_I2CDevice.h
@@ -7,7 +7,7 @@
 #ifndef ARD_I2C_DEVICE_H
 #define ARD_I2C_DEVICE_H
 
-#include <Wire.h>
+#include "device/platforms/arduino/Wire/Wire.h"		// TODO fix this later so it can actually find arduino libraries
 #include "device/I2CDevice.h"
 #include "device/platforms/arduino/ARD_Handlers.h"
 #include "sched/macros.h"
@@ -109,7 +109,107 @@ public:
 
 	RESET();
 	return RET_SUCCESS;
-
     }
 
+	/// @brief transmit to an I2C device
+    /// @param addr     the I2C address to write to
+    /// @param buff     the buffer to write
+    /// @param len      the size of 'buff' in bytes
+    /// @return
+	RetType write(I2CAddr_t &addr, uint8_t *buff, size_t len, uint32_t timeout) {
+		RESUME();
+
+		RetType ret = CALL(m_lock.acquire());
+        if (ret != RET_SUCCESS) {
+            // some error
+            RESET();
+            return ret;
+        }
+
+		m_blocked = sched_dispatched;
+
+		Wire.begin();
+		
+	}
+
+	/// @brief write to the I2C register
+    /// @param addr     the I2C address to write to
+    /// @param buff     the buffer to write
+    /// @param len      the size of 'buff' in bytes
+    /// @return
+	RetType receive(I2CAddr_t &addr, uint8_t *buff, size_t len, uint32_t timeout) {
+	}
 	
+	/// @brief read from the I2C register
+    ///        blocks until enough data is ready
+    /// @param addr     the I2C address to read from
+    /// @param buff     the buffer to read into
+    /// @param len      the number of bytes to read
+    /// @return
+	RetType read(I2CAddr_t &addr, uint8_t *buff, size_t len, uint32_t timeout) {	// Do I need the timeout? Arduino seems to automatically block
+		RESUME();
+
+		// block and wait for the device to be available
+        RetType ret = CALL(m_lock.acquire());
+        if (ret != RET_SUCCESS) {
+            // some error
+            RESET();
+            return ret;
+        }
+
+		m_blocked = sched_dispatched;
+
+		Wire.beginTransmission(addr.dev_addr);
+		Wire.write(addr.mem_addr);				
+		Wire.requestFrom(addr, len);
+		int i = 0;
+		while (Wire.available() && i < len) {
+			*(buff + i++) = Wire.read();
+		} 
+		Wire.endTransmission();
+
+		ret = CALL(m_lock.release());
+        if (ret != RET_SUCCESS) {
+            RESET();
+            return ret;
+        }
+
+		RESET();
+	}
+
+	/// @brief Request a transfer from the I2C slave
+    /// @param addr     the I2C address to read from
+    /// @param buff     the buffer to send command and store data in
+    /// @param inLen    the number of bytes to send
+    /// @param outLen   the number of bytes to receive
+    /// @return
+    RetType transmitReceive(I2CAddr_t &addr, uint8_t *buff, size_t inLen, size_t outLen, uint32_t timeout, uint8_t secondAddr) {
+	}
+
+	/// @brief called by I2C handler asynchronously
+    void callback(int) {
+        // all this does is set a flag
+        // the interrupt is actually "handled" in 'poll'
+        m_isr_flag = 1;
+    }
+
+	private:
+    // unique numbers for tx vs. rx callback
+    static const int TX_NUM = 0;
+    static const int RX_NUM = 1;
+
+    // currently blocked task
+    tid_t m_blocked;
+
+    // HAL I2C handle
+    I2C_HandleTypeDef *m_i2c;
+
+    // Device lock
+    BlockingSemaphore m_lock;
+
+    // Flag when an interrupt has occurred
+    uint8_t m_isr_flag;
+
+};
+
+#endif
