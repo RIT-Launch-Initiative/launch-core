22e61f0e817b414abd395fb2a583107d90a6e16a
Aaron Chan <aarchan108@gmail.com>
2023-08-14 23:57:08 -0400

    MAXM10S cleanup by removing static alloc, modify scoping and making it inherit from deivce

--- device/peripherals/MAXM10S/MAXM10S.h
+++ device/peripherals/MAXM10S/MAXM10S.h
@@ -9,60 +9,46 @@
  *
  * @link https://content.u-blox.com/sites/default/files/MAX-M10S_IntegrationManual_UBX-20053088.pdf
  */
-#ifndef LAUNCH_CORE_MAXM10S_H
-#define LAUNCH_CORE_MAXM10S_H
+#ifndef MAXM10S_H
+#define MAXM10S_H
 
-#include "device/I2CDevice.h"
 #include "return.h"
-#include "sched/macros/call.h"
+#include "sched/macros.h"
+#include "device/StreamDevice.h"
+#include "device/I2CDevice.h"
+#include "device/GPIODevice.h"
 
-
-/**
- * @brief The MAXM10S Registers
- */
-typedef enum {
-    /* The hight byte of the amount of data available from the sensor */
-    BYTE_COUNT_HIGH = 0xFD,
-    /* The low byte of the amount of data available from the sensor */
-    BYTE_COUNT_LOW = 0xFE,
-    /* The data stream register */
-    DATA_STREAM = 0xFF
-} MAXM10S_REG;
-
-/**
- * @brief Platform Independent Driver for the MAXM10S GPS module
- */
-class MAXM10S {
+class MAXM10S : public Device {
+public:
     static const uint8_t MAXM10S_I2C_ADDR = 0x42;
 
-public:
+    typedef enum {
+        /* The hight byte of the amount of data available from the sensor */
+        BYTE_COUNT_HIGH = 0xFD,
+        /* The low byte of the amount of data available from the sensor */
+        BYTE_COUNT_LOW = 0xFE,
+        /* The data stream register */
+        DATA_STREAM = 0xFF
+    } MAXM10S_REG;
+
     /**
      * @brief CTOR For MAXM10S
      * @param i2c_device the I2C device to use
      */
-    MAXM10S(I2CDevice &i2c_device, StreamDevice &stream_device, GPIODevice &reset_pin, GPIODevice &interrupt_pin) :
-    m_i2c(i2c_device), m_stream(stream_device),
-    m_reset_pin(reset_pin), m_interrupt_pin(interrupt_pin) {};
+    MAXM10S(I2CDevice &i2c_device, StreamDevice &stream_device, GPIODevice &reset_pin, GPIODevice &interrupt_pin, const char *device_name = "MAXM10S") :
+            Device(device_name), m_i2c(i2c_device), m_stream(stream_device), m_reset_pin(reset_pin),
+            m_interrupt_pin(interrupt_pin) {};
 
-    /**
-     * @brief Initialize the MAXM10S sensor
-     *
-     * @return RetType the scheduler status
-     */
-    RetType init() {
+    RetType init() override {
         RESUME();
 
         RetType ret = CALL(reset());
-        if (RET_SUCCESS != ret) {
-            RESET();
-            return ret;
+        if (RET_SUCCESS == ret) {
+            ret = CALL(m_interrupt_pin.set(0));
         }
 
-        ret = CALL(m_interrupt_pin.set(0));
-
-        init_end:
         RESET();
-        return RET_SUCCESS;
+        return ret;
     }
 
     /**
@@ -71,20 +57,17 @@ public:
      * @param buff the gps data buffer
      * @param buff_len the length of the buffer
      * @param bytes_available pointer to the number of bytes available to be read
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType read_data_rand_access(uint8_t *buff, size_t buff_len, size_t *bytes_available) {
         RESUME();
 
-
         RetType ret = CALL(get_amt_data(bytes_available));
-        if (*bytes_available == 0 || *bytes_available > buff_len) {
-            RESET();
-            return RET_ERROR;  // no data available or buffer too small
+        if (*bytes_available == 0 || *bytes_available > buff_len) { // No data available
+            ret = RET_ERROR;
+        } else {
+            ret = CALL(read_reg(DATA_STREAM, buff, *bytes_available));
         }
-
-        // read the data
-        ret = CALL(read_reg(DATA_STREAM, buff, *bytes_available));
 
         RESET();
         return ret;
@@ -94,16 +77,13 @@ public:
      * @brief Get the amount of data available from the sensor
      *
      * @param amt ptr to the amount of data available
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType get_amt_data(size_t *amt) {
         RESUME();
 
-        // read the amount of data available
-        static uint8_t byteCount[2];
-
-        RetType ret = CALL(read_reg(BYTE_COUNT_HIGH, byteCount, 2));
-        *amt = (byteCount[0] << 8) | byteCount[1];
+        RetType ret = CALL(read_reg(BYTE_COUNT_HIGH, m_buff, 2));
+        *amt = (m_buff[0] << 8) | m_buff[1];
 
         RESET();
         return ret;
@@ -113,7 +93,7 @@ public:
      * @brief Reads the amount of data available from the sensor
      * @details This is a current access read (stream read)
      * @param buff the gps data buffer
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType read_data_curr_access(uint8_t *buff) {  // TODO: Untested
         RESUME();
@@ -136,7 +116,7 @@ public:
      * @brief Requests and reads positional data
      * @details This is a current access read (stream read)
      * @param buff the buffer to read data into
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType uart_read_posllh_data(uint8_t *buff) {
         RESUME();
@@ -169,8 +149,8 @@ private:
     StreamDevice &m_stream;
     GPIODevice &m_reset_pin;
     GPIODevice &m_interrupt_pin;
-    /* The I2C address of the sensor */
     I2CAddr_t addr = {.dev_addr = MAXM10S_I2C_ADDR << 1, .mem_addr = 0, .mem_addr_size = 1};
+    uint8_t m_buff[2];
 
     /**
      * @brief Reads a register from the MAXM10S sensor
@@ -178,7 +158,7 @@ private:
      * @param reg the register to read from
      * @param buff the buffer to read into
      * @param numBytes the number of bytes to read
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType read_reg(MAXM10S_REG reg, uint8_t *buff, size_t numBytes) {
         RESUME();
@@ -187,7 +167,7 @@ private:
         RetType ret = CALL(m_i2c.transmit(addr, (uint8_t *) reg, 1, 150));
         if (RET_SUCCESS != ret) goto read_reg_end;
 
-        addr.mem_addr = static_cast<uint8_t>(reg);
+        addr.mem_addr = reg;
         ret = CALL(m_i2c.read(addr, buff, numBytes, 1500));
 
         read_reg_end:
@@ -200,7 +180,7 @@ private:
      *
      * @param cmdBuff The NMBA or UBX command to send
      * @param cmdLen The length of the command
-     * @return RetType the scheduler status
+     * @return Scheduler Status
      */
     RetType sendCommand(uint8_t *cmdBuff, int cmdLen) {
         RESUME();
@@ -214,4 +194,4 @@ private:
     }
 };
 
-#endif  // LAUNCH_CORE_MAXM10S_H
+#endif  // MAXM10S_H
