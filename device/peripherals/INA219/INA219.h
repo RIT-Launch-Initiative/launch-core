77a7d160136822c567a579000ca9b203ef13c0ee
Torin Samples <dragonclaw339@gmail.com>
2023-09-13 21:09:15 -0400

    starter

--- device/peripherals/INA219/INA219.h
+++ device/peripherals/INA219/INA219.h
@@ -8,19 +8,26 @@
 
 #ifndef INA219_H
 #define INA219_H
-#define INA219_DATA_STRUCT(variable_name) INA219_DATA_T variable_name = {.batteryCurrentAddress = 1000000, .railCurrentAddress3V3 = 1000100, .railCurrentAddress5V0 = 1000001, .shuntVoltage = 0, .busVoltage = 0, .power = 0, .current = 0}
+#define INA219_BATTERY_ADDR UINT8_C(0x40)
+#define INA219_3V3RAIL_ADDR UINT8_C(0x44)
+#define INA219_5V0RAIL_ADDR UINT8_C(0x41)
+
+#define INA219_DATA_STRUCT(variable_name) INA219_DATA_T variable_name = {.shuntVoltage = 0, .busVoltage = 0, .power = 0, .current = 0}
 
 using INA219_DATA_T = struct {
-    const uint16_t batteryCurrentAddress;
-    const uint16_t railCurrentAddress3V3;
-    const uint16_t railCurrentAddress5V0;
-    uint16_t shuntVoltage;
-    uint16_t busVoltage;
-    uint16_t power;
-    uint16_t current;
+    float shuntVoltage;
+    float busVoltage;
+    float power;
+    float current;
 };
 
 class INA219 : public Device {
-    public:
-    INA219(I2CDevice &i2cDev, const)
+public:
+    INA219(I2CDevice &i2cDev, const uint16_t address = INA219_BATTERY_ADDR, const char *name = "INA219") 
+            : Device(name), mI2C(&i2cDev),
+              i2cAddr({.dev_addr = static_cast<uint16_t>(address << 1), .mem_addr = 0, .mem_addr_size = 1}) {}
+
+private:
+    I2CDevice *mI2C;
+    I2CAddr_t i2cAddr;
 }
