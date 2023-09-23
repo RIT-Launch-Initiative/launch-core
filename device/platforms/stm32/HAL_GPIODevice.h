252f25d4c49b91192face93ada4913a7665e5448
Nate Aquino <naquino14@outlook.com>
2023-09-23 13:27:14 -0400

    Performed autoformatter pass on HAL_GPIODevice.h

--- device/platforms/stm32/HAL_GPIODevice.h
+++ device/platforms/stm32/HAL_GPIODevice.h
@@ -8,25 +8,23 @@
 #define LAUNCH_CORE_HALGPIODEVICE_H
 
 #ifdef STM32F446xx
-#include "stm32f4xx_hal_gpio.h"
-#include "stm32f4xx_hal_def.h"
 #include "stm32f446xx.h"
+#include "stm32f4xx_hal_def.h"
+#include "stm32f4xx_hal_gpio.h"
 #elif STM32L476xx
-#include "stm32l4xx_hal_gpio.h"
-#include "stm32l4xx_hal_def.h"
 #include "stm32l476xx.h"
+#include "stm32l4xx_hal_def.h"
+#include "stm32l4xx_hal_gpio.h"
 #endif
 
 #include "device/GPIODevice.h"
 #include "sched/sched.h"
 
 class HALGPIODevice : public GPIODevice {
-public:
-
-    HALGPIODevice(const char *name, GPIO_TypeDef *halGPIO, uint16_t pin) :
-                                                            GPIODevice(name),
-                                                            m_gpio(halGPIO),
-                                                            m_pin(pin) {};
+   public:
+    HALGPIODevice(const char *name, GPIO_TypeDef *halGPIO, uint16_t pin) : GPIODevice(name),
+                                                                           m_gpio(halGPIO),
+                                                                           m_pin(pin){};
 
     RetType init() override {
         return RET_SUCCESS;
@@ -43,7 +41,6 @@ public:
     RetType poll() override {
         return RET_SUCCESS;
     }
-
 
     RetType set(uint32_t val) override {
         if (!(val == 0 || val == 1)) {
@@ -62,9 +59,9 @@ public:
         return RET_SUCCESS;
     }
 
-private:
-    GPIO_TypeDef* m_gpio;
+   private:
+    GPIO_TypeDef *m_gpio;
     uint16_t m_pin;
 };
 
-#endif //LAUNCH_CORE_HALGPIODEVICE_H
+#endif  // LAUNCH_CORE_HALGPIODEVICE_H
